3df67b9d701b692029878c965c93875773f82648
kaizen3031593 <zen.conroy@gmail.com>
2021-12-07 16:57:08 -0500

    copy generate example code

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,30 @@
 const { typescript } = require('projen');
+
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'cdk-generate-synthetic-examples',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
-  // release: undefined,      /* Add release management to this project. */
+  srcdir: 'lib',
+  repository: 'https://github.com/cdklabs/cdk-generate-synthetic-examples',
+  authorEmail: 'aws-cdk-dev@amazon.com',
+  authorName: 'Amazon Web Servies',
+  authorOrganization: true,
+  description: 'Generate synthetic examples for CDK libraries',
+  deps: [
+    '@jsii/spec',
+    'jsii-reflect',
+    'jsii-rosetta',
+    'fs-extra',
+    'yargs',
+  ],
+  devDeps: [
+    '@types/jest',
+    '@types/yargs',
+    'jest',
+    'typescript',
+  ],
+  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+  releaseToNpm: true,
+  gitignore: ['*.js', '*.d.ts'],
 });
+
 project.synth();
