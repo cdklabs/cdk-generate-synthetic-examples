ddd0441911e66329432695024885ca04a02a2322
Kaizen Conroy <zen.conroy@gmail.com>
2022-08-15 11:33:07 -0400

    refactor: replaceAssembly is now imported from @jsii/spec

--- src/assemblies.ts
+++ src/assemblies.ts
@@ -1,15 +1,7 @@
 import * as path from 'path';
-import { Assembly, Type, writeAssembly, compressedAssemblyExists } from '@jsii/spec';
+import { Type } from '@jsii/spec';
 import * as fs from 'fs-extra';
 import { FIXTURE_NAME } from './generate-missing-examples';
-
-/**
- * Replaces the file where the original assembly file *should* be found with a new assembly file.
- * Detects whether or not there is a compressed assembly, and if there is, compresses the new assembly also.
- */
-export function replaceAssembly(assembly: Assembly, directory: string) {
-  writeAssembly(directory, _fingerprint(assembly), { compress: compressedAssemblyExists(directory) });
-}
 
 export function addFixtureToRosetta(directory: string, fileName: string, fixture: string) {
   const rosettaPath = path.join(directory, 'rosetta');
@@ -21,19 +13,6 @@ export function addFixtureToRosetta(directory: string, fileName: string, fixture
     return;
   }
   fs.writeFileSync(filePath, fixture);
-}
-
-/**
- * Replaces the old fingerprint with '***********'.
- *
- * @rmuller says fingerprinting is useless, as we do not actually check
- * if an assembly is changed. Instead of keeping the old (wrong) fingerprint
- * or spending extra time calculating a new fingerprint, we replace with '**********'
- * that demonstrates the fingerprint has changed.
- */
-function _fingerprint(assembly: Assembly): Assembly {
-  assembly.fingerprint = '*'.repeat(10);
-  return assembly;
 }
 
 /**
