92caa394e821cab9a399660df852738059a1134c
Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>
2023-06-01 16:00:45 -0400

    chore: migrate to cdklabs projen project type (#205)
    * migrate to cdklabs-projen-project-types
    
    * update cdklabs-projen-project-types
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,11 @@
-const { typescript } = require('projen');
+import { CdklabsTypeScriptProject } from 'cdklabs-projen-project-types';
 
-const project = new typescript.TypeScriptProject({
+const project = new CdklabsTypeScriptProject({
+  projenrcTs: true,
+  private: false,
   defaultReleaseBranch: 'main',
   name: 'cdk-generate-synthetic-examples',
   repository: 'https://github.com/cdklabs/cdk-generate-synthetic-examples',
-  authorEmail: 'aws-cdk-dev@amazon.com',
-  authorName: 'Amazon Web Servies',
-  authorOrganization: true,
   description: 'Generate synthetic examples for CDK libraries',
   deps: [
     '@jsii/spec',
@@ -17,20 +16,19 @@ const project = new typescript.TypeScriptProject({
     'yargs',
   ],
   devDeps: [
+    'cdklabs-projen-project-types',
     '@types/jest',
     '@types/yargs',
     'jest',
     '@types/fs-extra',
     'typescript',
   ],
-  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
   releaseToNpm: true,
   gitignore: ['*.js', '*.d.ts'],
-  autoApproveUpgrades: true,
-  autoApproveOptions: {
-    allowedUsernames: ['cdklabs-automation'],
-    secret: 'GITHUB_TOKEN',
-  },
+  workflowNodeVersion: '16.x',
+  minNodeVersion: '16.0.0',
+  enablePRAutoMerge: true,
+  setNodeEngineVersion: false,
 });
 
 project.synth();
